e72255167d929fa1a41a80e261a16fcc1fba929f
Johannes Zemlin <johannes.zemlin@mailbox.org>
2026-04-25 20:04:12 +0200

    basic oracle support

---
+++ core/src/test/java/ai/koryki/kql/DuckDBEngineTest.java
@@ -0,0 +1,70 @@
+package ai.koryki.kql;
+
+import ai.koryki.databases.FileAsserter;
+import ai.koryki.databases.cases.CSVAssert;
+import ai.koryki.databases.cases.StableFormatInfo;
+import ai.koryki.databases.northwind.NorthwindService;
+import ai.koryki.databases.northwind.duckdb.NorthwindDuckdb;
+import ai.koryki.duckdb.SqlQueryRenderer;
+import ai.koryki.iql.LinkResolver;
+import ai.koryki.jdbc.ColumnInfo;
+import ai.koryki.jdbc.Database;
+import ai.koryki.jdbc.ListResult;
+import ai.koryki.scaffold.Util;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class DuckDBEngineTest {
+
+    public static final String NORTHWIND_ROOT = "src/test/resources/ai/koryki/kql/northwind";
+    public static final String SUFFIX = ".kql";
+
+    private static LinkResolver resolver;
+    private static Database<ListResult<ColumnInfo>> database;
+    private static Engine<ColumnInfo, ListResult<ColumnInfo>> engine;
+
+    @BeforeAll
+    public static void readNorthwindDB() throws IOException {
+
+        resolver = NorthwindService.resolver();
+        database = NorthwindDuckdb.northwind();
+        engine = new Engine<>(database, resolver, new SqlQueryRenderer(), StableFormatInfo::new);
+    }
+
+    static Stream<Path> testFiles() throws IOException {
+
+        return Files.walk(Path.of(NORTHWIND_ROOT), FileVisitOption.FOLLOW_LINKS)
+                .filter(p -> p.toString().endsWith(SUFFIX));
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("testFiles")
+    void testEachFile(Path kql) throws IOException {
+
+        test(kql);
+    }
+
+    private static void test(Path kql) throws IOException {
+
+        Path sibling = FileAsserter.getSibling(kql, SUFFIX, ".csv");
+        File expectedFile = sibling.toFile();
+
+        String query = Files.readString(kql);
+        CSVAssert csv = new CSVAssert(engine, query, kql.toFile().getName());
+        String result = csv.getCsv();
+        if (expectedFile.canRead()) {
+            String expected = Files.readString(sibling);
+            csv.check(result, expected);
+        } else {
+            Util.text(result, expectedFile);
+        }
+    }
+}
