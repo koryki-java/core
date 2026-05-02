e62a51c097fdd1cd4e68ecc1f613ddaff0de2921
Johannes Zemlin <johannes.zemlin@mailbox.org>
2026-05-02 08:25:08 +0200

    fix, read duckdb from resource

--- northwind/src/main/java/ai/koryki/databases/northwind/duckdb/NorthwindDuckdb.java
+++ northwind/src/main/java/ai/koryki/databases/northwind/duckdb/NorthwindDuckdb.java
@@ -4,12 +4,20 @@ package ai.koryki.databases.northwind.duckdb;
 import ai.koryki.duckdb.DuckdbDatabase;
 import ai.koryki.jdbc.ResultProcessor;
 
+import java.nio.file.Path;
+import java.sql.Connection;
+
 public class NorthwindDuckdb {
 
-    public static String DUCKDB = "../northwind/src/main/resources/ai/koryki/databases/northwind/duckdb/northwind.duckdb";
+    public static String DUCKDB = "/ai/koryki/databases/northwind/duckdb/northwind.duckdb";
 
     public static <P extends ResultProcessor<?>> DuckdbDatabase<P> northwind() {
 
-        return new DuckdbDatabase<>( "northwind", DuckdbDatabase.connection(DUCKDB));
+        return new DuckdbDatabase<>( "northwind", fromResource(DUCKDB));
     }
+
+    public static Connection fromResource(String resource) {
+        return DuckdbDatabase.fromResource(resource, Path.of("/tmp/korykiai.duckdb"));
+    }
+
 }
